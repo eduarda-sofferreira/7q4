d26591899b5d61c61cf4eb9e30f9812414a5b093
eduarda-sofferreira <espf1@aluno.ifal.edu.br>
2024-12-21 19:44:40 -0300

    Create 7q4.js

---
+++ 7q4.js
@@ -0,0 +1,5 @@
+var numusuario = parseInt (prompt("Digite um número"))
+
+for (var i=1; i <= 10; i++) {
+    console.log("a tabuada desse número é" + numusuario + "x" + i + "=" + (numusuario * i))
+}
